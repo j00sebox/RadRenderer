78c06eb552a1be92780ebca4bbd411b69d678a66
josharms00 <josharms00@gmail.com>
2020-02-19 14:13:30 -0500

    Added addition and subtraction test set for matrix.

--- MyGraphicsEngine/UnitTests/matrix_functions.cpp
+++ MyGraphicsEngine/UnitTests/matrix_functions.cpp
@@ -38,7 +38,7 @@ TEST_F(MatrixTest, multiply) {
 				{ 3,	 3,		 3,		 2,		 { {13.0f, 12.0f, 5.0f}, {3.0f, 0.0f, 2.0f}, {17.0f, 2.0f, 1.0f} },			{ {4.0f, 6.0f}, {7.0f, 8.0f}, {19.0f, 2.0f} },		 { {231.0f, 184.0f}, {50.0f, 22.0f}, {101.0f, 120.0f} } },
 
 				// Test 2: Same dimensions. Shows that multiplication works with two matrices with the same dimesnions in this case two 4x4 ones
-				{ 4,	 4,		 4,		 4,		 { {67.0f, 5.0f, 5.0f, 7.0f}, {3.0f, 0.0f, 2.0f, 8.5f}, {17.6f, 2.3f, 1.1f, 99.0f}, {17.6f, 2.3f, 1.1f, 99.0f} },		 { {4.0f, 6.0f, 4.0f, 6.0f}, {7.0f, 8.8f, 3.3f, 5.0f}, {19.0f, 2.0f, 66.0f, 4.4f}, {1.0f, 2.0f, 3.0f, 4.0f } },		 { {405.0f, 470.0f, 635.5f, 477.0f}, {58.5f, 39.0f, 169.5f, 60.8f}, {206.4f, 326.04f, 447.59f, 517.94}, {206.4f, 326.04f, 447.59f, 517.94} } }
+				{ 4,	 4,		 4,		 4,		 { {67.0f, 5.0f, 5.0f, 7.0f}, {3.0f, 0.0f, 2.0f, 8.5f}, {17.6f, 2.3f, 1.1f, 99.0f}, {17.6f, 2.3f, 1.1f, 99.0f} },		 { {4.0f, 6.0f, 4.0f, 6.0f}, {7.0f, 8.8f, 3.3f, 5.0f}, {19.0f, 2.0f, 66.0f, 4.4f}, {1.0f, 2.0f, 3.0f, 4.0f } },		 { {405.0f, 470.0f, 635.5f, 477.0f}, {58.5f, 39.0f, 169.5f, 60.8f}, {206.4f, 326.04f, 447.59f, 517.94f}, {206.4f, 326.04f, 447.59f, 517.94f} } }
 		}
 	};
 
@@ -58,6 +58,60 @@ TEST_F(MatrixTest, multiply) {
 	}
 }
 
-//TEST_F(MatrixTest, add) {
-//
-//}
+TEST_F(MatrixTest, addandsubtract) {
+
+	// Define all the params needed for the tests
+	struct MultiplyTest
+	{
+		int row1;
+		int col1;
+		int row2;
+		int col2;
+		std::vector< std::vector<float> > test_param_1;
+		std::vector< std::vector<float> > test_param_2;
+		std::vector< std::vector<float> > expected_res;
+		bool add;
+
+	};
+
+	const int TEST_CASES = 2;
+
+	// Initialize array with each element holding the testing params for a case
+	std::array< MultiplyTest, TEST_CASES > test_arr =
+	{
+		{
+			//	row1	col1	row2	col2												test_param_1																												test_param_2																													expected_res													add
+
+				// Test 1: Same dimensions addition
+				{ 4,	 4,		 4,		 4,		 { {67.0f, 5.0f, 5.0f, 7.0f}, {3.0f, 0.0f, 2.0f, 8.5f}, {17.6f, 2.3f, 1.1f, 99.0f}, {17.6f, 2.3f, 1.1f, 99.0f} },		 { {4.0f, 6.0f, 4.0f, 6.0f}, {7.0f, 8.8f, 3.3f, 5.0f}, {19.0f, 2.0f, 66.0f, 4.4f}, {1.0f, 2.0f, 3.0f, 4.0f } },		 { {71.0f, 11.0f, 9.0f, 13.0f}, {10.0f, 8.8f, 5.3f, 13.5f}, {36.6f, 4.3f, 67.1f, 103.4f}, {18.6f, 4.3f, 4.1f, 103.0f} },	true },
+
+				// Test 2: Same dimensions subtraction
+				{ 4,	 4,		 4,		 4,		 { {67.0f, 5.0f, 5.0f, 7.0f}, {11.0f, 90.0f, 4.6f, 8.5f}, {21.4f, 2.3f, 1.1f, 99.0f}, {17.6f, 2.3f, 3.1f, 99.0f} },		 { {4.0f, 4.0f, 4.0f, 6.0f}, {7.0f, 8.8f, 3.3f, 5.0f}, {19.0f, 2.0f, 1.1f, 4.4f}, {1.0f, 2.0f, 3.0f, 4.0f } },		 { {63.0f, 1.0f, 1.0f, 1.0f}, {4.0f, 81.2f, 1.3f, 3.5f}, {2.4f, 0.3f, 0.0f, 94.6f}, {16.6f, 0.3f, 0.1f, 95.0f} },	false }
+		}
+	};
+
+	// loop through test cases
+	for (int i = 0; i < TEST_CASES; i++)
+	{
+		Matrix test_p1(test_arr[i].row1, test_arr[i].col1);
+		Matrix test_p2(test_arr[i].row2, test_arr[i].col2);
+		Matrix res(test_arr[i].row1, test_arr[i].col1);
+
+		test_p1 = test_arr[i].test_param_1;
+		test_p2 = test_arr[i].test_param_2;
+
+		if (test_arr[i].add) {
+			 res = test_p1 + test_p2;
+		} 
+		else {
+			 res = test_p1 - test_p2;
+		}
+
+		for (int k = 0; k < test_arr[i].row1; k++)
+		{
+			for(int j = 0; j < test_arr[i].col1; j++)
+				// Evalute if this is true
+				EXPECT_NEAR(res(k, j), test_arr[i].expected_res[k][j], 0.01);
+		}
+	}
+}
